fb652505accc9232dfe8ace8f3b80202593ed3e3
Ayush Khatri <khatriayush2003@gmail.com>
2024-10-07 18:46:13 +0530

    Updated clerk route

--- app/api/clerk/route.ts
+++ app/api/clerk/route.ts
@@ -1,10 +1,11 @@
+import prisma from '@/prisma/client'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { headers } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
 import { Webhook } from 'svix'
-
 const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || ``
 
-async function validateRequest(request: Request) {
+async function validateRequest(request: NextRequest) {
   const payloadString = await request.text()
   const headerPayload = headers()
 
@@ -17,19 +18,49 @@ async function validateRequest(request: Request) {
   return wh.verify(payloadString, svixHeaders) as WebhookEvent
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   try {
     const payload = await validateRequest(request)
-    console.log(payload)
+    console.log(payload);
+
+    let user = null;
+
+    const clerkId = payload.data?.id;
+    console.log(clerkId);
+    if (!clerkId)
+      return NextResponse.json(
+        { error: 'No user ID provided' },
+        { status: 400 },
+      );
+
 
     // process the event
+    if (payload.type === 'user.created') {
+      user = await prisma.user.upsert({
+        where: {
+          clerkId,
+        },
+        update: {
+          clerkId,
+        },
+        create: {
+          clerkId,
+        },
+      });
+    }
 
-    // everything went well
-    return Response.json({ message: 'Received' })
+    if (payload.type === 'user.deleted') {
+      user = await prisma.user.delete({
+        where: {
+          clerkId,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: 'Received' }, { status: 200 });
   } catch (e) {
-    // something went wrong
-    // no changes were made to the database
-    return Response.error()
+    console.error(e);
+    return NextResponse.json({ error: 'Something went wrong' }, { status: 500 });
   }
 }
 
